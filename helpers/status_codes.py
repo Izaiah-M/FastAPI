f377032bd59cb7536deff8acb82639665633d4a4
iz.aia.h <imukisa024@gmail.com>
2023-08-06 23:20:24 +0300

    status codes

--- helpers/status_codes.py
+++ helpers/status_codes.py
@@ -3,6 +3,8 @@ from fastapi.responses import JSONResponse
 
 def STATUS_CODE_200(data=None):
     return JSONResponse(content={"message": "Success", "data": data}, status_code=200)
+
+
 STATUS_CODE_400 = JSONResponse(content={"message": "Bad Request"}, status_code=400)
 STATUS_CODE_MISSING_FIELDS_400 = JSONResponse(content={"message": "Missing Fields!"}, status_code=400)
 STATUS_CODE_404 = JSONResponse(content={"message": "Not Found"}, status_code=404)
